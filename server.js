8975bee388c75af56453bc8894cc0ad4983769cb
Robin JOSEPH <robin82joseph@gmail.com>
2022-11-21 11:23:31 +0100

    dish endpoints made

--- server.js
+++ server.js
@@ -1,15 +1,59 @@
-import express, { json } from 'express'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.use(json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-    }
-)
+mongoose.connect("mongodb://localhost:27017/test")
 
-app.listen(30001, () => {
-    console.log('Server started on port 30001')
-    }
-)
+
+const Dish = mongoose.model("Dish", {
+    id : Number,
+    name: String,
+    description: String,
+    price: Number,
+    allergens: [String]
+})
+
+// Create a dish
+app.post("/dishes", (req, res) => {
+    const dishToSave = new Dish(req.body)
+    dishToSave.save().then((dish) => res.json(dish))
+})
+
+// Get all dishes
+app.get("/dishes", (req, res) => {
+    Dish.find().then((dishes) => res.json(dishes))
+    .catch(() => res.status(404).end())
+})
+
+// Get a single dish
+app.get("/dishes/:id", async(req, res) => {
+    Dish.findById(req.params.id)
+    .then((dish) => res.json(dish))
+    .catch(() => res.status(404).end())
+})
+
+// Update one by ID
+app.put("/dishes/:id", async(req, res) => {
+    Dish.findByIdAndUpdate(req.params.id, req.body)
+    .then((dish) => res.json(dish))
+    .catch(() => res.status(404).end())
+})
+
+// Delete one by ID
+app.delete("/dishes/:id", async(req, res) => {
+    Dish.findByIdAndDelete(req.params.id)
+    .then((dish) => res.json(dish))
+    .catch(() => res.status(404).end())
+})
+
+app.get("*", (req, res) => {
+    res.status(404).end()
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})    
